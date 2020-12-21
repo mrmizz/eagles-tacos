a69771019133f779a583f13fcb4c9d931bce399e
Alexander Binaei <alex.binaei@carpe.io>
2020-12-20 17:06:59 -0800

    border

--- src/View/Order/Order.elm
+++ src/View/Order/Order.elm
@@ -24,7 +24,7 @@ body =
             [ class "hero-body has-bg-color2"
             ]
             [ Html.div
-                [ class "container is-fluid has-bg-color1"
+                [ class "container is-fluid has-bg-color1 has-border-all"
                 ]
                 [ Html.h1
                     [ class "title is-1 has-text-danger has-text-centered"
